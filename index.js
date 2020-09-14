7ccbae20e768fecf2f930dd6222c6656c8f2f82a
Ofer Shany <ofers@cellwize.local>
2020-09-14 11:12:11 +0300

    Parsing yaml into an object

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict'
 
 const core = require('@actions/core')
+const yaml = require('js-yaml')
 const { promises: fs } = require('fs')
 
 const main = async () => {
   const path = core.getInput('path')
   const content = await fs.readFile(path, 'utf8')
   console.log(content)
+  let config = yaml.safeLoad(content)
+  console.log(config)
   core.setOutput('appId', 'TEST')
 }
 
