49274dc14a2de62aecf86ec47f2acc8ac416e71d
Ofer Shany <ofers@cellwize.local>
2020-09-14 14:15:52 +0300

    Calling chime app registration endpoint

--- index.js
+++ index.js
@@ -2,19 +2,24 @@
 
 const core = require('@actions/core')
 const yaml = require('js-yaml')
+const got = require('got')
 const { promises: fs } = require('fs')
 
 const main = async () => {
   const path = core.getInput('config')
+  const chimeurl = core.getInput('chimeurl')
   const content = await fs.readFile(path, 'utf8')
   console.log(content)
   console.log('==============================')
   let config = yaml.safeLoad(content)
   console.log(config)
   console.log('==============================')
-  console.log(config.application.schema[0].name)
-  console.log(config.application.schema[0].displayName)
-  core.setOutput('appId', 'TEST')
+  
+  const body = await got.post(chimeurl+'/chime/v1/apps', {
+        json: config.application
+    }).json();
+  console.log(body);
+  core.setOutput('appId', body.id)
 }
 
 main().catch(err => core.setFailed(err.message))
